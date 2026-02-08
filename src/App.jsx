f23c6e37c6033d948b3c98ea8b4af50fcb495784
PavanPochi06 <pavanpochi06@gmail.com>
2026-02-08 11:23:05 +0530

    Add main sections and Navbar component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Navbar } from "@/layout/Navbar";
+import { Hero } from "@/sections/Hero";
+import { About } from "@/sections/About";
+import { Experience } from "@/sections/Experience";
+import { Project } from "@/sections/Projects";
+import { Testimonials } from "@/sections/Testimonials";
+import { Contact } from "@/sections/Contact";
+
 function App() {
   return <div className="min-h-screen overflow-x-hidden">
     <Navbar />
-    <main></main>
+    <main>
+      <Hero />
+      <About />
+      <Experience />
+      <Project />
+      <Testimonials />
+      <Contact />
+    </main>
   </div>
 }
 
